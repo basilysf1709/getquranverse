70bcb4cbc3a3820de51846030ce5435d376d3b0f
Basil Yusuf - WSL <basilyusuf1709@gmail.com>
2024-02-22 01:33:24 -0500

    frontend complete ???

--- app/host/page.tsx
+++ app/host/page.tsx
@@ -1,18 +1,25 @@
-
+'use client'
+import { useRouter } from "next/navigation";
 
 export default function Host() {
+  const router = useRouter();
+  const handleSubmit = (event: any) => {
+    event.preventDefault();
+    router.push("/lobby");
+  };
+
   return (
-    <form className="flex justify-center items-center flex-col h-screen w-screen">
+    <form onSubmit={handleSubmit} className="flex justify-center items-center flex-col h-screen w-screen">
       <div className="mb-5 w-3/4">
         <label
-          htmlFor="email"
+          htmlFor="username"
           className="block mb-2 text-sm font-medium text-white"
         >
           Your Username
         </label>
         <input
-          type="email"
-          id="email"
+          type="username"
+          id="username"
           className="w-full shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-purple-500 focus:border-purple-500 block p-2.5"
           placeholder="Asfandyaar Talhat"
           required
@@ -26,4 +33,4 @@ export default function Host() {
       </div>
     </form>
   );
-};
+}
--- app/lobby/page.tsx
+++ app/lobby/page.tsx
@@ -1,5 +1,51 @@
+'use client'
+import { useRouter } from "next/navigation";
+import { CiCircleCheck } from "react-icons/ci";
+
 export default function Lobby() {
+  const router = useRouter()
+  const participants = [
+    { name: "Asfandyaar Talhat", imageSrc: "/assets/1.png" },
+    { name: "Ahmad Farag", imageSrc: "/assets/2.png" },
+    { name: "Sauudagar", imageSrc: "/assets/3.png" },
+    { name: "Basil Yusuf", imageSrc: "/assets/4.png" },
+    { name: "Ahmed Yusuf", imageSrc: "/assets/5.png" },
+  ];
+
+  const handleOnClick = (event : any) => {
+    event.preventDefault()
+    router.push("/game")
+  }
   return (
-    <h1>asdfas</h1>
-  )
+    <div className="h-screen w-full flex justify-center items-center flex-col">
+      <h2 className="text-left mb-4 text-4xl font-extrabold dark:text-white">Waiting Lobby:</h2>
+      <ul className="w-3/4 py-8 px-16 divide-y divide-gray-200">
+      {participants.map((participant, index) => (
+          <li key={index} className="py-3 sm:pt-4">
+          <div className="flex items-center space-x-4 rtl:space-x-reverse">
+            <div className="flex-shrink-0">
+              <img
+                className="w-8 h-8 rounded-full"
+                src={participant.imageSrc}
+                alt={participant.name}
+              />
+            </div>
+            <div className="flex-1 min-w-0">
+              <p className="text-sm font-medium truncate">{participant.name}</p>
+            </div>
+            <div className="inline-flex items-center text-base font-semibold">
+              <CiCircleCheck size={30} fill="rgb(126 34 206)" />
+            </div>
+          </div>
+        </li>
+        ))}
+      </ul>
+      <button
+          onClick={handleOnClick}
+          className="w-7/12 m-4 text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-purple-300 font-medium rounded-lg text-sm text-center mx-4 py-2.5"
+        >
+          Join Game Session
+        </button>
+    </div>
+  );
 }
