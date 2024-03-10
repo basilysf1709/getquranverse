8e83a6c4f5a70bf04a6f4222a31de8685fa7d0f5
Basil Yusuf - WSL <basilyusuf1709@gmail.com>
2024-03-10 01:50:10 -0500

    done

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -18,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <Analytics/>
+      <SpeedInsights/>
       <body className={inter.className}>{children}</body>
     </html>
   );
