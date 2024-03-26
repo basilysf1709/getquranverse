ded69754ae156305f07eb897347ebd51ef66e624
Basil Yusuf <basilyusuf1709@gmail.com>
2024-03-26 12:10:05 -0400

    minor visual changes

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import { Testimonials } from "@/components/Testimonials";
 export default function Home() {
   return (
     <main className="flex justify-center items-center flex-col h-screen">
-      <h1 className="p-8 text-4xl font-extrabold leading-none tracking-tight text-white">Ramadan Mubarak 🎉</h1>
+      <h1 className="p-8 text-4xl font-extrabold leading-none tracking-tight text-white">Assalamua'laikum 🎉</h1>
       <JoinGame />
       <HostGame />
       <SoloGame />
