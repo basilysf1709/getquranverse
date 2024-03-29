25d617e6f4ae067703db7353a64ffe2a0c908b68
Basil Yusuf <basilyusuf1709@gmail.com>
2024-03-28 23:02:24 -0400

    done

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,12 @@ import { Testimonials } from "@/components/Testimonials";
 export default function Home() {
   return (
     <main className="flex justify-center items-center flex-col h-screen">
-      <h1 className="p-8 text-4xl font-extrabold leading-none tracking-tight text-white">Assalamua&apos;laikum 🎉</h1>
+      <h1 className="p-8 text-4xl font-extrabold leading-none tracking-tight text-white">
+        Quran Verse 🎉
+        <sub className="text-xs border border-gray-600 rounded px-1 align-baseline">
+          Beta
+        </sub>
+      </h1>
       <JoinGame />
       <HostGame />
       <SoloGame />
